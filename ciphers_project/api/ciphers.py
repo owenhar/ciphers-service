77cb735e25dcf6105d3fe3d8c9271fa0b6a17d36
Owen Harris <harriowe@iu.edu>
2024-02-27 19:21:38 -0500

    added support for uppercase and lowercase

--- ciphers_project/api/ciphers.py
+++ ciphers_project/api/ciphers.py
@@ -1,9 +1,14 @@
 def caesar_encode(plain_text, shift):
     cipher_text = ""
     for c in plain_text:
-        c_encoded = ord(c) + shift
-        if (c_encoded > ord('z')): 
-            c_encoded = c_encoded - 26
+        if (ord(c) >= ord('a')):
+            c_encoded = ord(c) + shift
+            if (c_encoded > ord('z')): 
+                c_encoded = c_encoded - 26
+        else:
+            c_encoded = ord(c) + shift
+            if (c_encoded > ord('Z')): 
+                c_encoded = c_encoded - 26
         c_encoded = chr(c_encoded)
         cipher_text += c_encoded
     return cipher_text
