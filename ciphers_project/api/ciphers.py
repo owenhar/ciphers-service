6a421f63c12baf9504e41b26e873ba184ac8abf6
Owen Harris <harriowe@iu.edu>
2024-02-27 19:07:59 -0500

    finished for now

---
+++ ciphers_project/api/ciphers.py
@@ -0,0 +1,9 @@
+def caesar_encode(plain_text, shift):
+    cipher_text = ""
+    for c in plain_text:
+        c_encoded = ord(c) + shift
+        if (c_encoded > ord('z')): 
+            c_encoded = c_encoded - 26
+        c_encoded = chr(c_encoded)
+        cipher_text += c_encoded
+    return cipher_text
---
+++ ciphers_project/api/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .ciphers import caesar_encode
+
+# Create your views here.
+
+def greetings(request):
+    result = {"message": "Welcome to the ciphers service! -- Owen Harris"}
+    return JsonResponse(result)
+
+def encode(request, plaintext, shift):
+    parameters = dict(request.GET)
+    print(parameters)
+    cipher = caesar_encode(plaintext, shift)
+    return JsonResponse({"cipher": cipher})
